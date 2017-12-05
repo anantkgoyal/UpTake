fb1d27b56da2caab8957161097c2c5d7af9431c1
Anant <anantkgoyal@gmail.com>
2017-12-05 09:05:04 -0600

    Set up the initial exploration

--- src/Tests/Test1.java
+++ src/Tests/Test1.java
@@ -1,5 +1,29 @@
 package Tests;
 
-public class Test1 {
+import java.io.File;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		String sourcePath = System.getProperty("user.dir");
+		File file = new File(sourcePath + "/lib/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.uptake.com/");
+		
+		WebElement firstName = driver.findElement(By.id("firstname-c7ed7b27-03cc-49ef-a175-ecef13772c93"));
+		firstName.sendKeys("Anant");
+		
+		WebElement demoRequest = driver.findElement(By.xpath("//*[contains(text(), 'Request a Demo')]"));
+		demoRequest.click();
+				
+		
+	}
 }
